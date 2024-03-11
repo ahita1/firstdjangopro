0aa7082ef2b71e6ed7f95762f3628ed0c37f3b1c
ahita1 <ahitafani833@gmail.com>
2024-03-11 07:04:02 -0700

    creating a vote sections

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,10 @@
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render  ,get_object_or_404
-from .models import Question
+from .models import Question, Choice
+from django.urls import reverse
+from django.db.models import F
+
+
 
 
 def index(request):
@@ -18,3 +22,27 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
+
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
