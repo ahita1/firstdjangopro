de968ca3ba62d9d48ce0367153aa8e7eec2d7f79
ahita1 <ahitafani833@gmail.com>
2024-03-11 06:27:19 -0700

    add new features haha

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
-
+from django.shortcuts import render  ,get_object_or_404
 from .models import Question
 
 
@@ -10,9 +9,12 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 # ...
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
